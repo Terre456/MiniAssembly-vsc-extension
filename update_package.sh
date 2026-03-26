a423d63558a83e5bfa0127e1446240892f19df55
Boris <sabredeboris@gmail.com>
2026-03-26 10:56:37 +0100

    added package updater

---
+++ update_package.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+FILE=$(ls | grep .vsix | sort -Vr | head -1)
+MAJOR=$(echo $FILE | cut -d '.' -f 1 | cut -d '-' -f 2)
+MEDIUM=$(echo $FILE | cut -d '.' -f 2)
+MINOR=$(echo $FILE | cut -d '.' -f 3)
+case $1 in
+    major) MAJOR=$((MAJOR + 1))
+    MEDIUM=0
+    MINOR=0;;
+    medium) MEDIUM=$((MEDIUM + 1))
+    MINOR=0;;
+    *)MINOR=$((MINOR + 1))
+esac
+new_file=miniassembly-$MAJOR.$MEDIUM.$MINOR.vsix
+echo $new_file
+vsce package
+mv miniassembly-0.0.1.vsix $new_file
